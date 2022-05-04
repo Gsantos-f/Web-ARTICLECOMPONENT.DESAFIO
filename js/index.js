6f866a3396e9cfe042993a55857b88896b8f3939
Gabriel dos Santos <gsantos.4dev@gmail.com>
2022-05-04 11:08:19 -0300

    enter

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 function clicou() {
-    // document.querySelector('.share-redes-sociais').style.display = "flex"
     document.querySelector('.share-redes-sociais').classList.add('aberto')
 }
 
+// const share = document.querySelector('.share-redes-sociais')
+// const body = document.querySelector('main')
+// body.addEventListener('click', tirou())
+
+// function tirou(){
+//     share.classList.remove('aberto')
+// }
